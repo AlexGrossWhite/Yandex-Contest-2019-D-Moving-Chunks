9533ce0eb80374b2d94000042a2160c3cd2733ef
AlexGrossWhite <112899421+AlexGrossWhite@users.noreply.github.com>
2023-10-17 20:02:49 +0300

    Initial commit

---
+++ D Moving Chunks/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] ints = Console.ReadLine().Split(' ').Select(o => Int32.Parse(o)).ToArray();
+List<int> result = new List<int>();
+int[] chunks = Console.ReadLine().Split(' ').Select(o => Int32.Parse(o)).ToArray();
+for (int i = 0; i < ints[2]; i++)
+{
+    int[] request = Console.ReadLine().Split(' ').Select(o => Int32.Parse(o)).ToArray();
+    for (int j = request[2] - 1; j <= request[3] - 1; j++)
+    {
+        if (chunks[j] != request[0])
+        {
+            result.Add(0);
+            break;
+        }
+    }
+    if (result.Count < i + 1) result.Add(1);
+    if (result[^1] == 1)
+    {
+        for (int j =  request[2] - 1; j <= request[3] - 1; j++)
+        {
+            chunks[j] = request[1];
+        }
+    }
+}
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
